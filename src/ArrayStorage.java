ac1f3c25d3879fc2e26a0f825313ee55e00ea452
t-olyaolya <tyulyakovaolga@gmail.com>
2018-05-13 14:54:06 +0400

    delete searchNull

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,28 +6,18 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    int searchNull () {
-        int index = 0;
-        for (int i = 0; i <= storage.length; i++) {
-            if (storage [i] == null) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    }
 
     void clear() {
         Arrays.fill(storage,null);
     }
 
     void save(Resume r) {
-       storage [searchNull()] = r;
+       storage [size()] = r;
     }
 
 
     Resume get(String uuid) {
-        for (int r = 0; r < searchNull(); r++) {
+        for (int r = 0; r < size(); r++) {
             if (storage[r].toString() == uuid) {
                 return storage[r];
             }
@@ -36,7 +26,7 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int r = 0; r <  searchNull(); r++) {
+        for (int r = 0; r <  size(); r++) {
             if (storage[r].uuid.equals(uuid)) {
                 System.arraycopy(storage, r+1, storage, r, storage.length-r-1);
             }
@@ -48,10 +38,17 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, searchNull());
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
-        return  searchNull();
+        int index = 0;
+        for (int i = 0; i <= storage.length; i++) {
+            if (storage [i] == null) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 }
