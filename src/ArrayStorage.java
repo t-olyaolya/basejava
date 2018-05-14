ae4fc9b6209badb2ae6888625d9566e1f08bb0fb
t-olyaolya <tyulyakovaolga@gmail.com>
2018-05-14 21:39:50 +0400

    size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,25 +1,25 @@
 import java.util.Arrays;
-　
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int sizeStorage = 0;
-　
+
     void clear() {
         Arrays.fill(storage,null);
         sizeStorage = 0;
     }
-　
+
     void save(Resume r) {
        if (sizeStorage < storage.length) {
         storage [sizeStorage] = r;
         sizeStorage++;
        }
     }
-　
-　
+
+
     Resume get(String uuid) {
         for (int r = 0; r < sizeStorage; r++) {
             if (storage[r].uuid.equals(uuid)) {
@@ -28,29 +28,28 @@ public class ArrayStorage {
         }
         return null;
     }
-　
+
     void delete(String uuid) {
-        Resume[] storageTmp = new Resume[storage.length];
         for (int r = 0; r < sizeStorage; r++) {
             if (storage[r].uuid.equals(uuid)) {
-                System.arraycopy(storage, r+1, storage, r, storage.lenhth-r-1);
+                System.arraycopy(storage, r+1, storage, r, storage.length-r-1);
                 storage[sizeStorage-1] = null;
                 sizeStorage--;
                 break;
             }
         }
-　
+
     }
-　
+
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, sizeStorage);
     }
-　
+
     int size() {
         return sizeStorage;
     }
 }
-　
+
